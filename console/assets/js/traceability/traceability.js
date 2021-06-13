3e1ebae5ba1791eaf3dbad6f9c819265c18e1166
Keegan Skeate <keeganskeate@gmail.com>
2021-06-13 11:48:13 -0500

    Create/access secret w/ test 🔐 Traceability UI

--- console/assets/js/traceability/traceability.js
+++ console/assets/js/traceability/traceability.js
@@ -2,13 +2,55 @@
  * Traceability JavaScript | Cannlytics Console
  * Author: Keegan Skeate
  * Created: 6/12/2021
- * Updated: 6/12/2021
+ * Updated: 6/13/2021
  */
+
+import { authRequest, showNotification } from '../utils.js';
+import { theme } from '../settings/theme.js';
 
 export const traceability = {
 
   initialize() {
     console.log('Initializing traceability...');
+  },
+
+
+  getEmployees(tableId) {
+    /*
+     * Get employees from the Metrc API, through the Cannlytics API,
+     * and render the data in a table.
+     */
+    
+    // Define the columns for the table.
+    const columnDefs = [
+      { field: 'FullName', sortable: true, filter: true },
+      { field: 'License', sortable: true, filter: true },
+    ];
+
+    // Specify the table options.
+    const gridOptions = {
+      columnDefs: columnDefs,
+      pagination: true,
+      // rowSelection: 'multiple',
+      suppressRowClickSelection: false,
+      // singleClickEdit: true,
+      // onRowClicked: event => console.log('A row was clicked'),
+      onGridReady: event => theme.toggleTheme(theme.getTheme()),
+    };
+
+    // Get the data.
+    authRequest(`/api/traceability/employees`).then((data) => {
+      console.log('Table data:', data); // DEV:
+      const eGridDiv = document.querySelector(`#${tableId}`);
+      new agGrid.Grid(eGridDiv, gridOptions);
+      gridOptions.api.setRowData(data);
+    });
+
+    // TODO: Attach export functionality
+    //function exportTableData() {
+    //  gridOptions.api.exportDataAsCsv();
+    //}
+
   },
 
 
