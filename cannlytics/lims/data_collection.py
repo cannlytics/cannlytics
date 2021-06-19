fb762085214df59ba9a15fd2c8cdfd23b03dd5d4
Keegan Skeate <keeganskeate@gmail.com>
2021-06-19 16:58:24 -0500

    Beginning to touch up data collection doc strings

--- cannlytics/lims/data_collection.py
+++ cannlytics/lims/data_collection.py
@@ -4,40 +4,48 @@ Authors:
   Keegan Skeate <keegan@cannlytics.com>
   Charles Rice <charles@ufosoftwarellc.com>
 Created: 6/15/2021
-Updated: 6/15/2021
+Updated: 6/19/2021
 """
+
+import pandas as pd
 
 
 def get_sample_name(df):
-
-    import pandas as pd
-
-    # convert the first column of the first sheet to lowercase
+    """Return the sample name from a dictionary.
+    Converts the first column of the first sheet to lowercase.
+    Args:
+        df (DataFrame): A DataFrame.
+    Returns:
+        (dict): The sample name as a key, value pair.
+    """
     df['Sheet1'].ObjClass = df['Sheet1'].ObjClass.str.lower()
-
-    # put the samplename key and samplename value into a dictionary
     samples = dict(df['Sheet1'][df['Sheet1'].ObjClass == 'samplename'].values)
-
     return samples
 
 
 def get_compound_df(df):
-
-    import pandas as pd
-
-   # rename the columns in the Compound sheet to match the required names
+    """ rename the columns in the Compound sheet to match the required names
+    for simplicty make a copy of the Compound sheet
+    handle NaN values
+    Args:
+        df (DataFrame): A DataFrame.
+    Returns:
+        (dict): 
+    """
     df['Compound'].rename(columns = {'Name':'analyte', 'Amount':'measurement'}, inplace = True)
-
-    # for simplicty make a copy of the Compound sheet
     df_compound = df['Compound'].copy()
-
-    # handle NaN values
     df_compound.loc[(df_compound.analyte.isnull()) & (df_compound.measurement > 0), 'analyte'] = 'wildcard'
     df_compound.dropna(subset = ['analyte'], inplace = True)
-
     return df_compound
 
+
 def clean_special_characters(df, column_name):
+    """
+    Args:
+        df (DataFrame): A DataFrame.
+    Returns:
+        (dict): 
+    """
     df[column_name] = df[column_name].str.strip()
     df[column_name] = df[column_name].str.rstrip('.)]')
     df[column_name] = df[column_name].str.replace('%',  'percent', regex=True)
@@ -58,52 +66,59 @@ def clean_special_characters(df, column_name):
 
 
 def import_agilent_gc_residual_solvments(file_name):
-
-    import pandas as pd
-
-    #read in all the excel sheets at one time
+    """read in all the excel sheets at one time
+    get the sample name
+    get and cleanup the compound df from the compound sheet
+    add the analyte names and measurements to an array of dictionaries and add that to the main array
+    Args:
+        df (DataFrame): A DataFrame.
+    Returns:
+        (dict): 
+    """
     df = pd.read_excel(file_name, sheet_name = None)
-
-    # get the sample name
     samples = get_sample_name(df)
-
-    # get and cleanup the compound df from the compound sheet
     df_compound = get_compound_df(df)
-
-    # add the analyte names and measurements to an array of dictionaries and add that to the main array
     samples['metrics'] = df_compound[['analyte', 'measurement']].to_dict('records')
-
     return samples
 
 
 def import_agilent_gc_terpenes(file_name):
-
-    import pandas as pd
-
-    #read in all the excel sheets at one time
+    """read in all the excel sheets at one time
+    get the sample name
+    get and cleanup the compound df from the compound sheet
+    replace special characters
+    add the analyte names and measurements to an array of dictionaries and add that to the main array
+    Args:
+        df (DataFrame): A DataFrame.
+    Returns:
+        (dict): 
+    """
     df = pd.read_excel(file_name, sheet_name = None)
-
-    # get the sample name
     samples = get_sample_name(df)
-
-    # get and cleanup the compound df from the compound sheet
     df_compound = get_compound_df(df)
-
-    # replace special characters
     df_compound = clean_special_characters(df_compound, 'analyte')
-
-    # add the analyte names and measurements to an array of dictionaries and add that to the main array
     samples['metrics'] = df_compound[['analyte', 'measurement']].to_dict('records')
-
     return samples
 
-# This is the same as residual solvents, added the function in case it needs to be customized
+
 def import_agilent_cannbinoids(file_name):
+    """This is the same as residual solvents, added the function
+    in case it needs to be customized.
+    Args:
+        df (DataFrame): A DataFrame.
+    Returns:
+        (dict): 
+    """
     return import_agilent_gc_residual_solvments(file_name)
 
-def import_metals(file_name):
 
-    import pandas as pd
+def import_metals(file_name):
+    """
+    Args:
+        df (DataFrame): A DataFrame.
+    Returns:
+        (dict): 
+    """
 
     # read in the log sheet and summary sheets seperately to parsing easier
     log_df = pd.read_excel(file_name, sheet_name = 'Log')
@@ -118,10 +133,9 @@ def import_metals(file_name):
     # get list of samples
     sample_ids = log_df['Sample ID'].tolist()
 
+    # Parse measurements.
     samples = []
     measurements = {}
-
-
     for sample_id in sample_ids:
         sample = {}
         sample[ 'sample_id' ] = sample_id
@@ -141,8 +155,3 @@ def import_metals(file_name):
         samples.append(measurements)
     
     return samples
-
-
-
-
-
