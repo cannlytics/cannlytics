a1d5815814993c68bee43b31a542e4a57c87d40b
Keegan Skeate <keeganskeate@gmail.com>
2023-01-01 10:38:55 -0800

    Adding stable functionality to CCRS module

--- cannlytics/data/ccrs/__init__.py
+++ cannlytics/data/ccrs/__init__.py
@@ -1,13 +1,27 @@
 """
 CCRS Client Initialization | Cannlytics
-Copyright (c) 2021-2022 Cannlytics
+Copyright (c) 2021-2023 Cannlytics
 
 Authors: Keegan Skeate <https://github.com/keeganskeate>
 Created: 11/6/2021
-Updated: 9/22/2022
+Updated: 1/1/2023
 """
-from .ccrs import CCRS
+from .ccrs import (
+    CCRS,
+    get_datafiles,
+    unzip_datafiles,
+    merge_datasets,
+    format_test_value,
+    find_detections,
+    format_lab_results,
+)
 
 __all__ = [
     CCRS,
+    get_datafiles,
+    unzip_datafiles,
+    merge_datasets,
+    format_test_value,
+    find_detections,
+    format_lab_results,
 ]
--- cannlytics/data/ccrs/ccrs.py
+++ cannlytics/data/ccrs/ccrs.py
@@ -11,6 +11,8 @@ License: <https://github.com/cannlytics/cannlytics/blob/main/LICENSE>
 # Standard imports:
 import csv
 import os
+from typing import List, Optional
+from zipfile import ZipFile
 
 # External imports:
 import pandas as pd
@@ -21,7 +23,189 @@ from cannlytics.data.ccrs.constants import (
     CCRS_ANALYSES,
     CCRS_DATASETS,
 )
-from cannlytics.utils.utils import snake_case
+from cannlytics.utils.utils import (
+    convert_to_numeric,
+    rmerge,
+    snake_case,
+    sorted_nicely,
+)
+
+
+def get_datafiles(
+        data_dir: str,
+        dataset: Optional[str] = '',
+        desc: Optional[bool] = True,
+    ) -> list:
+    """Get all CCRS datafiles of a given type in a directory."""
+    datafiles = sorted_nicely([
+        os.path.join(data_dir, f, f, f + '.csv')
+        for f in os.listdir(data_dir) if f.startswith(dataset)
+    ])
+    if desc:
+        datafiles.reverse()
+    return datafiles
+
+
+def unzip_datafiles(
+        data_dir: str,
+        verbose: Optional[bool] = True,
+    ) -> None:
+    """Unzip all CCRS datafiles in a given directory."""
+    zip_files = [f for f in os.listdir(data_dir) if f.endswith('.zip')]
+    for zip_file in zip_files:
+        filename = os.path.join(data_dir, zip_file)
+        zip_dest = filename.rstrip('.zip')
+        if not os.path.exists(zip_dest):
+            os.makedirs(zip_dest)
+        zip_ref = ZipFile(filename)
+        zip_ref.extractall(zip_dest)
+        zip_ref.close()
+        os.remove(filename)
+        if verbose:
+            print('Unzipped:', zip_file)
+
+
+def merge_datasets(
+        df: pd.DataFrame,
+        datafiles: List[str],
+        dataset: str,
+        on: Optional[str] = 'id',
+        target: Optional[str] = '',
+        how: Optional[str] = 'left',
+        sep: Optional[str] = '\t',
+        validate: Optional[str] = 'm:1',
+        rename: Optional[dict] = None,
+        on_bad_lines: Optional[str] = 'skip',
+    ) -> pd.DataFrame:
+    """Merge a supplemental CCRS dataset to an existing dataset.
+    Note: `IsDeleted` and `UnitWeightGrams` are treated as strings.
+    Lab results cannot be split between datafiles.
+    """
+    n = len(df)
+    augmented = pd.DataFrame()
+    fields = CCRS_DATASETS[dataset]['fields']
+    parse_dates = CCRS_DATASETS[dataset]['date_fields']
+    usecols = list(fields.keys()) + parse_dates
+    dtype = {k: v for k, v in fields.items() if v != 'datetime64'}
+    # FIXME: These fields throw `ValueError` if not strings.
+    if dtype.get('IsDeleted'):
+        dtype['IsDeleted'] = 'string'
+    if dtype.get('UnitWeightGrams'):
+        dtype['UnitWeightGrams'] = 'string'
+    if dtype.get('TestValue'):
+        dtype['TestValue'] = 'string'
+    for datafile in datafiles:
+        supplement = pd.read_csv(
+            datafile,
+            sep=sep,
+            encoding='utf-16',
+            engine='python',
+            parse_dates=parse_dates,
+            dtype=dtype,
+            usecols=usecols,
+            on_bad_lines=on_bad_lines,
+        )
+        if rename is not None:
+            supplement.rename(rename, axis=1, inplace=True)
+        # FIXME: Handle lab results that may be split between datafiles.
+        if dataset == 'lab_results':
+            supplement = format_lab_results(df, supplement)
+        match = rmerge(
+            df,
+            supplement,
+            on=on,
+            how=how,
+            validate=validate,
+        )
+        matched = match.loc[~match[target].isna()]
+        augmented = pd.concat([augmented, matched]) 
+        if len(augmented) == n:
+            break
+    return augmented
+
+
+def format_test_value(tests, compound, value_key='TestValue'):
+    """Format a lab result test value from a DataFrame of tests."""
+    value = tests.loc[(tests.key == compound)]
+    try:
+        return convert_to_numeric(value.iloc[0][value_key])
+    except:
+        return None
+
+
+def find_detections(
+        tests,
+        analysis,
+        analysis_key='type',
+        analyte_key='key',
+        value_key='TestValue',
+    ) -> List[str]:
+    """Find compounds detected for a given analysis from given tests."""
+    df = tests.loc[tests[analysis_key] == analysis]
+    if df.empty:
+        return []
+    df.loc[:, value_key] = df[value_key].apply(convert_to_numeric)
+    detected = df.loc[df[value_key] > 0]
+    if detected.empty:
+        return []
+    return detected[analyte_key].to_list()
+
+
+def format_lab_results(
+        df: pd.DataFrame,
+        results: pd.DataFrame,
+        item_key: Optional[str] = 'InventoryId',
+        analysis_name: Optional[str] = 'TestName',
+        analysis_key: Optional[str] = 'TestValue',
+    ) -> pd.DataFrame:
+    """Format CCRS lab results to merge into another dataset."""
+
+    # Curate lab results.
+    analyte_data = results[analysis_name].map(CCRS_ANALYTES).values.tolist()
+    results = results.join(pd.DataFrame(analyte_data))
+    results['type'] = results['type'].map(CCRS_ANALYSES)
+
+    # Find lab results for each item.
+    formatted = []
+    item_ids = list(df[item_key].unique())
+    for item_id in item_ids:
+        item_results = results.loc[results[item_key].astype(str) == item_id]
+        if item_results.empty:
+            continue
+
+        # Map certain test values.
+        values = item_results.iloc[0].to_dict()
+        [values.pop(key) for key in [analysis_name, analysis_key]]
+        entry = {
+            **values,
+            'analyses': list(item_results['type'].unique()),
+            'delta_9_thc': format_test_value(item_results, 'delta_9_thc'),
+            'thca': format_test_value(item_results, 'thca'),
+            'total_thc': format_test_value(item_results, 'total_thc'),
+            'cbd': format_test_value(item_results, 'cbd'),
+            'cbda': format_test_value(item_results, 'cbda'),
+            'total_cbd': format_test_value(item_results, 'total_cbd'),
+            'moisture_content': format_test_value(item_results, 'moisture_content'),
+            'water_activity': format_test_value(item_results, 'water_activity'),
+        }
+
+        # Determine `status`.
+        statuses = list(item_results['LabTestStatus'].unique())
+        if 'Fail' in statuses:
+            entry['status'] = 'Fail'
+        else:
+            entry['status'] = 'Pass'
+
+        # Determine detected contaminants.
+        entry['pesticides'] = find_detections(item_results, 'pesticides')
+        entry['residual_solvents'] = find_detections(item_results, 'residual_solvent')
+        entry['heavy_metals'] = find_detections(item_results, 'heavy_metals')
+
+        # Record the lab results for the item.
+        formatted.append(entry)
+    
+    # Return the lab results.
+    return pd.DataFrame(formatted)
 
 
 class CCRS(object):
