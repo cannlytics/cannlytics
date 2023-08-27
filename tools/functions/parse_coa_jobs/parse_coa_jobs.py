a22e16c16be9b7c61f1868d13ac5a12ac6f0600a
Keegan Skeate <keeganskeate@gmail.com>
2023-08-26 22:55:22 -0700

    Working on parse COA jobs cloud function

--- tools/functions/parse_coa_jobs/parse_coa_jobs.py
+++ tools/functions/parse_coa_jobs/parse_coa_jobs.py
@@ -4,7 +4,7 @@ Copyright (c) 2022 Cannlytics
 
 Authors: Keegan Skeate <https://github.com/keeganskeate>
 Created: 8/25/2023
-Updated: 8/25/2023
+Updated: 8/26/2023
 License: MIT License <https://github.com/cannlytics/cannlytics/blob/main/LICENSE>
 
 Description:
@@ -46,48 +46,53 @@ def parse_coa_jobs(event: Event[Change[DocumentSnapshot]]) -> None:
     # Get a dictionary representing the document
     uid = event.params['uid']
     job_id = event.params['job_id']
-    job_information = event.data.to_dict()
-
-    # Access a particular field as you would any dictionary
-    name = new_value["name"]
+    print('User:', uid, 'Job:', job_id)
 
     # TODO: Read Cannlytics API key?
-
-    # Get job and user details.
-    uid = event.params['uid']
-    job_id = event.params['job_id']
-    print('User:', uid, 'Job:', job_id)
 
     # If the document is new, it's a new job being created.
     print("New job being created")
     start_time = datetime.now()
 
-    # Make a request to parse the COA.
-    coa_file_path = document.get('coa_file_path')  # Assuming this is in the document.
-    # TODO: Implement logic to request the parsing service.
-    response = requests.post("YOUR_PARSING_ENDPOINT_URL", data={"file_path": coa_file_path})
+    # Get the COA URL.
+    job_information = event.data.to_dict()
+    coa_url = job_information.get('job_file_url')
+    print('COA URL:', coa_url)
+    if coa_url is None:
+        print('No COA URL provided')
+        return
     
-    end_time = datetime.now()
+    # Make a request to parse the COA.
+    api_url = 'https://cannlytics.com/api/data/coas'
+    response = requests.post(api_url, data={'urls': [coa_url]})
+    print('Response:', response.status_code)
+    try:
+        lab_result_data = response.json()
+    except:
+        lab_result_data = {}
 
     # Once we have the response, update the job.
-    if response.status_code == 200:  # Assuming 200 means success.
-        lab_result_data = response.json()
-        db.collection('users').document(uid).collection('parse_coa_jobs').document(job_id).update({
-            "job_finished_at": end_time.isoformat(),
-            "job_error": False,
-            "job_finished": True,
-            "job_duration_seconds": (end_time - start_time).seconds,
+    end_time = datetime.now()
+    if response.status_code == 200:
+        job_data = {
+            'job_finished_at': end_time.isoformat(),
+            'job_error': False,
+            'job_finished': True,
+            'job_duration_seconds': (end_time - start_time).seconds,
             **lab_result_data
-        })
-        print("Job finished successfully")
+        }
+        print('Job finished successfully')
     else:
         error_message = response.text
-        db.collection('users').document(uid).collection('parse_coa_jobs').document(job_id).update({
-            "job_finished_at": end_time.isoformat(),
-            "job_error": True,
-            "job_error_message": error_message,
-            "job_finished": True,
-            "job_duration_seconds": (end_time - start_time).seconds
-        })
-        print(f"Job failed with error: {error_message}")
-     
+        job_data = {
+            'job_finished_at': end_time.isoformat(),
+            'job_error': True,
+            'job_error_message': error_message,
+            'job_finished': True,
+            'job_duration_seconds': (end_time - start_time).seconds
+        }
+        print(f'Job failed with error: {error_message}')
+
+    # Save the data to Firestore.
+    ref = db.collection('users').document(uid).collection('parse_coa_jobs').document(job_id)
+    ref.set(job_data, merge=True)
