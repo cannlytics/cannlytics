85eacd2a226522741e75fa266b1bbf7a7e65e660
Keegan Skeate <keeganskeate@gmail.com>
2023-09-27 13:04:59 -0700

    Minor refactor to get results MD

--- datasets/cannabis_tests/algorithms/get_results_md.py
+++ datasets/cannabis_tests/algorithms/get_results_md.py
@@ -4,9 +4,8 @@ Copyright (c) 2023 Cannlytics
 
 Authors:
     Keegan Skeate <https://github.com/keeganskeate>
-    Candace O'Sullivan-Sutherland <https://github.com/candy-o>
-Created: 8/13/2023
-Updated: 8/13/2023
+Created: 9/26/2023
+Updated: 9/27/2023
 License: CC-BY 4.0 <https://huggingface.co/datasets/cannlytics/cannabis_tests/blob/main/LICENSE>
 
 Description:
@@ -15,19 +14,30 @@ Description:
 
 Data Sources:
     
-    - Public records request from the Maryland Cannabis Administration (MCA).
+    - Public records request from the Maryland Medical Cannabis Commission (MMCC).
 
 """
+# Standard imports:
+from datetime import datetime
 import os
 import re
+
+# External imports:
+from cannlytics.utils import snake_case
 import numpy as np
 import pandas as pd
 import pdfplumber
-from datetime import datetime
-from cannlytics.utils import snake_case
 
 
+# Constants.
 STATE = 'MD'
+
+
+# === PRR PDF Extraction ===
+
+def extract_producer_id(label):
+    """Extracts the producer ID from a Metrc label."""
+    return label[7:13]
 
 
 def extract_info(line):
@@ -57,11 +67,6 @@ def extract_info(line):
     result_match = re.search(r'(\d+(\.\d+)?)', rest_of_line)
     result = result_match.group(1) if result_match else None
     return tag, category, test_name, test_passed, result
-
-
-def extract_producer_id(label):
-    """Extracts the producer ID from a Metrc label."""
-    return label[7:13]
 
 
 def extract_data_from_pdf(pdf_path):
@@ -220,6 +225,8 @@ def standardize_data(df, output_filename):
     print(f"Data standardized and saved: {output_filename}")
 
 
+# === PRR CSV Extraction ===
+
 def get_unique_analytes(df):
     """Get a list of all analytes."""
     unique_analytes = set()
@@ -229,14 +236,11 @@ def get_unique_analytes(df):
     return sorted(list(unique_analytes))
 
 
-def standardize_long_results():
+def get_results_md(data_dir, stats_dir=None):
     """Standardize the long results data."""
-
-    data_dir = r"D:\data\maryland\prr"
 
     # Aggregate all lab results.
     all_data = []
-
     for dirpath, _, filenames in os.walk(data_dir):
         for filename in filenames:
             if filename.endswith('.csv'):
@@ -244,26 +248,20 @@ def standardize_long_results():
                 df = pd.read_csv(filepath)
                 all_data.append(df)
 
-    # Combine all dataframes (if necessary)
+    # Combine all dataframes.
     df = pd.concat(all_data, ignore_index=True)
 
-    # TODO: Standardize the data.
-
-    # Pivot the dataframe
+    # Pivot the dataframe.
     pivot_df = df.pivot_table(
-        index=['TestYear', 'PackageId', 'StrainName', 'TestingFacilityId',], # 'date_tested'
+        index=['TestYear', 'PackageId', 'StrainName', 'TestingFacilityId',],
         columns='TestTypeName',
         values='TestResultLevel',
         aggfunc='first'
     ).reset_index()
 
-    # Determine the "status" based on the "date_tested" column
+    # Determine the "status" based on the "TestPassed" column.
     status = df.groupby('PackageId')['TestPassed'].apply(lambda x: 'Fail' if False in x.values else 'Pass')
     pivot_df = pivot_df.merge(status, left_on='PackageId', right_index=True)
-
-    # # Isolate a specific test.
-    # test = 'Total Yeast and Mold Count (CFU/g) Raw Plant Material'
-    # ym = pivot_df[pivot_df[test].notna()]
 
     # Standardize the columns.
     std = pd.DataFrame(pivot_df)
@@ -276,7 +274,7 @@ def standardize_long_results():
     for analyte in analytes:
         std[analyte] = np.nan
 
-    # FIXME: Get the result for each analyte.
+    # Get the result for each analyte.
     skip_columns = ['PackageId', 'TestingFacilityId', 'TestPassed',
                     'TestYear', 'StrainName']
     for index, row in pivot_df.iterrows():
@@ -300,19 +298,28 @@ def standardize_long_results():
     # std['lab_id'] = pivot_df['TestingFacilityId']
 
     # Save the results.
-    outfile = "../data/md/raw/md-lab-results-2023-09-27.csv"
-    std.to_csv(outfile, index=False)
-    print(f"Data standardized and saved: {outfile}")
+    if stats_dir:
+        date = datetime.now().strftime("%Y-%m-%d")
+        std.to_csv(os.path.join(stats_dir, f'md-lab-results-{date}.csv'), index=False)
+        std.to_csv(os.path.join(stats_dir, f'md-lab-results-latest.csv'), index=False)
+        print(f"Data saved: {os.path.join(stats_dir, f'md-lab-results-{date}.csv')}")
+
+    # Return the data.
+    return std
 
 
 # === Test ===
 # [✓] Tested: 2023-09-27 by Keegan Skeate <keegan@cannlytics>
 if __name__ == "__main__":
 
-    # Extract all data.
-    pdf_path = "../data/md/raw/public-records-request-md-2023-06-30.pdf"
-    extract_data_from_pdf(pdf_path)
+    # # Extract all data from the PRR PDF.
+    # pdf_path = "../data/md/raw/public-records-request-md-2023-06-30.pdf"
+    # extract_data_from_pdf(pdf_path)
+    # dataset_folder = os.path.join(os.path.dirname(pdf_path), ".datasets")
+    # aggregate_datasets(dataset_folder, "../data/md/raw/md-lab-results-2023-06-30.csv")
 
-    # Aggregate the data.
-    dataset_folder = os.path.join(os.path.dirname(pdf_path), ".datasets")
-    aggregate_datasets(dataset_folder, "../data/md/raw/md-lab-results-2023-06-30.csv")
+    # Aggregate Maryland results from the PRR CSVs.
+    data_dir = r'D:\data\maryland\prr'
+    stats_dir = '../data/md'
+    md_results = get_results_md(data_dir, stats_dir)
+    print('Aggregated %i MD lab results.' % len(md_results))
