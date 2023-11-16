7ab225a5bca1141f0e128e72c7fd5f76846a68ff
Keegan Skeate <keeganskeate@gmail.com>
2023-11-15 22:41:16 -0800

    Development of TerpLife Labs COA parsing

--- cannlytics/data/coas/algorithms/terplife.py
+++ cannlytics/data/coas/algorithms/terplife.py
@@ -196,7 +196,7 @@ def parse_terplife_coa(
         front_page_text = front_page_text.replace(extra, '')
 
     # Process the text.
-    front_page_text = front_page_text.replace('█', '\n')    
+    front_page_text = front_page_text.replace('█', '\n')
     lines = front_page_text.split('\n')
     lines = [x for x in lines if x != '']
 
@@ -355,7 +355,7 @@ def parse_terplife_coa(
             text = page.extract_text()
 
             # Extract pesticide data.
-            if 'Pesticides' in text:
+            if 'Pesticides' in text and 'Heavy Metals & Pesticides' not in text:
                 analyses.append('pesticides')
                 midpoint = page.width * 0.475
                 left_half = (0, 0, midpoint, page.height)
@@ -388,7 +388,6 @@ def parse_terplife_coa(
                         'units': 'ppb',
                     })
                 continue
-
 
             # Extract mycotoxins data.
             if 'Mycotoxins' in text:
@@ -584,11 +583,11 @@ def parse_terplife_coa(
     # Extract the results on the front page.
     if 'Terpenes Summary' in front_page_text:
         results_text = front_page_text.split('Terpenes Summary')[-1].split('Terpenes Summary')[0]
-        results_lines = results_text.split('Analyte Dilution')[-1].split('\n')[2:]
+        results_lines = results_text.split('Analyte Dilution')[-1].replace('Total', '\nTotal').split('\n')[2:]
     else:
         results_text = front_page_text.split('Result\n')[-1].split('Total THC')[0]
         results_lines = results_text.split('\n')[1:]
-    results_lines = [x.strip() for x in results_lines if x]
+    results_lines = [x.strip() for x in results_lines if x.strip()]
     for line in results_lines:
 
         # Skip totals.
@@ -600,6 +599,8 @@ def parse_terplife_coa(
             continue
         elif 'Total Cannabinoids' in line:
             break
+        elif 'Terpene results are' in line:
+            break
 
         # Extract a result, if not already extracted.
         first_value = find_first_value(line)
@@ -609,6 +610,8 @@ def parse_terplife_coa(
         key = snake_case(name)
         key = ANALYTES.get(key, key)
         values = [x.strip() for x in line[first_value:].split(' ') if x]
+        if len(values) == 2:
+            values.insert(0, None)
         analysis = None
         if name in cannabinoids:
             analysis = 'cannabinoids'
@@ -626,6 +629,9 @@ def parse_terplife_coa(
 
     # Close the report.
     report.close()
+
+    # TODO: Calculate moisture content if possible using dry and wet
+    # concentrations if the full report is not available.
 
     # Turn dates to ISO format.
     date_columns = [x for x in obs.keys() if x.startswith('date')]
@@ -660,17 +666,20 @@ if __name__ == '__main__':
 
     # [ ] TEST: Identify LIMS.
     parser = CoADoc(lims={'TerpLife Labs': TERPLIFE_LABS})
-    # doc = 'D:/data/florida/lab_results/.datasets/pdfs/terplife/T302229%20TLMB0216202301.pdf'
-    # lims = parser.identify_lims(doc)
-    # assert lims == 'TerpLife Labs'
-
-    # [ ] TEST: Parse a full-panel COA.
     doc = 'D:/data/florida/lab_results/.datasets/pdfs/terplife/T302229 TLMB0216202301.pdf'
-    # coa_data = parse_terplife_coa(parser, doc)
+    lims = parser.identify_lims(doc)
+    assert lims == 'TerpLife Labs'
+    print('Identified LIMS as', lims)
 
+    # [✓] TEST: Parse a full-panel COA.
+    doc = 'D:/data/florida/lab_results/.datasets/pdfs/terplife/T302229 TLMB0216202301.pdf'
+    coa_data = parse_terplife_coa(parser, doc)
+    print('Parsed full-panel COA:', doc)
 
     # [ ] TEST: Parse a cannabinoid and terpene COA.
     doc = 'D:/data/florida/lab_results/.datasets/pdfs/terplife/BU310823-2327TT.pdf'
+    coa_data = parse_terplife_coa(parser, doc)
+    print('Parsed cannabinoid and terpene COA:', doc)
 
     # [ ] TEST: Parse a R&D COA.
     doc = 'D:/data/florida/lab_results/.datasets/pdfs/terplife/BU180222-6925CKC.pdf'
@@ -690,12 +699,16 @@ if __name__ == '__main__':
     pdfs = [os.path.join(pdf_dir, x) for x in os.listdir(pdf_dir) if x.endswith('.pdf')]
     start = datetime.now()
     for pdf in pdfs:
-        # try:
-        coa_data = parse_terplife_coa(parser, pdf, verbose=True)
-        all_data.append(coa_data)
-        print('Parsed:', pdf)
-        # except:
-        #     print('Error:', pdf)
+        try:
+            coa_data = parse_terplife_coa(
+                parser, pdf,
+                save_to_firebase=True,
+                verbose=True,
+            )
+            all_data.append(coa_data)
+            print('Parsed:', pdf)
+        except:
+            print('Error:', pdf)
 
     # Calculate parsing statistics.
     end = datetime.now()
@@ -703,221 +716,10 @@ if __name__ == '__main__':
     print('Total Parsing Time:', end - start)
     print('Average Parsing Time per COA:', (end - start) / len(all_data))
 
-    # Analyze the data.
-    df = pd.DataFrame(all_data)
-    print(df['product_size'].unique())
-
     # Save the data.
+    df = pd.DataFrame(all_data)
     timestamp = datetime.now().strftime('%Y-%m-%d')
-    outfile = f'D:/data/florida/lab_results/.datasets/terplife-labs-coa-data{timestamp}.xlsx'
+    outfile = f'D:/data/florida/lab_results/.datasets/terplife-labs-coa-data-{timestamp}.xlsx'
     parser.save(df, outfile)
     print('Saved %i lab results to %s' % (len(df), outfile))
-    
 
-
-    # === DEV === 
-
-    # # Initialize the parser.
-    # parser = CoADoc()
-
-    # # Get the front page text.
-    # report = pdfplumber.open(doc)
-    # front_page = report.pages[0]
-    # front_page_text = report.pages[0].extract_text()
-
-    # # Clean extraneous text from the front page.
-    # extras = [
-    #     'Unless otherwise stated all quality control samples',
-    #     ' performed within specifications established by the Laboratory.'
-    # ]
-    # front_page_text = front_page_text.split('The data contained')[0]
-    # for extra in extras:
-    #     front_page_text = front_page_text.replace(extra, '')
-    # print(front_page_text)
-
-    # # Process the text.
-    # front_page_text = front_page_text.replace('█', '\n')    
-    # lines = front_page_text.split('\n')
-    # lines = [x for x in lines if x != '']
-
-    # # Optional: Initialize Firebase.
-    # try:
-    #     from cannlytics import firebase
-    #     from firebase_admin import get_app
-    #     firebase.initialize_firebase()
-    #     app = get_app()
-    #     bucket_name = f'{app.project_id}.appspot.com'
-    # except:
-    #     pass
-
-    # # Save the product image.
-    # try:
-    #     image_index, _ = max(
-    #         enumerate(front_page.images[image_starting_index:image_ending_index]),
-    #         key=lambda img: img[1]['width'] * img[1]['height']
-    #     )
-    #     image_data = parser.get_pdf_image_data(front_page, image_index=image_index + image_starting_index)
-    #     image_filename = doc.split('/')[-1].replace('.pdf', '.png')
-    #     image_file = os.path.join(image_dir, image_filename)
-    #     parser.save_image_data(image_data, image_file)
-    #     image_ref = f'data/lab_results/images/terplife/{image_filename}'
-    #     firebase.upload_file(image_ref, image_file, bucket_name=bucket_name)
-    #     image_url = firebase.get_file_url(image_ref, bucket_name=bucket_name)
-    #     obs['image_url'] = image_url
-    # except:
-    #     obs['image_url'] = None
-
-
-    # # TODO: Handle different length pages.
-    # number_of_pages = len(report.pages)
-
-
-    # === Parse with AI ===
-    from openai import OpenAI
-    from dotenv import dotenv_values
-
-
-    # Instructional prompt.
-    INSTRUCTIONAL_PROMPT = 'Return JSON.'
-
-    
-    # Prompt to parse metadata from the first page.
-    COA_PROMPT = """Given text, extract any of the following fields you see as JSON. Fields:
-
-    | Field | Example | Description |
-    |-------|---------|-------------|
-    | `analyses` | ["cannabinoids"] | A list of analyses performed on a given sample. |
-    | `status` | "pass" | The overall pass, fail, or N/A status for pass / fail analyses.   |
-    | `methods` | [{"analysis: "cannabinoids", "method": "HPLC"}] | The methods used for each analysis. |
-    | `date_collected` | 2022-04-20T04:20 | An ISO-formatted time when the sample was collected. |
-    | `date_tested` | 2022-04-20T16:20 | An ISO-formatted time when the sample was tested. |
-    | `date_received` | 2022-04-20T12:20 | An ISO-formatted time when the sample was received. |
-    | `lab` | "MCR Labs" | The lab that tested the sample. |
-    | `lab_address` | "85 Speen St, Framingham, MA 01701" | The lab's address. |
-    | `lab_street` | "85 Speen St" | The lab's street. |
-    | `lab_city` | "Framingham" | The lab's city. |
-    | `lab_state` | "MA" | The lab's state. |
-    | `lab_zipcode` | "01701" | The lab's zipcode. |
-    | `distributor` | "Fred's Dispensary" | The name of the product distributor, if applicable. |
-    | `distributor_address` | "420 State Ave, Olympia, WA 98506" | The distributor address, if applicable. |
-    | `distributor_street` | "420 State Ave" | The distributor street, if applicable. |
-    | `distributor_city` | "Olympia" | The distributor city, if applicable. |
-    | `distributor_state` | "WA" | The distributor state, if applicable. |
-    | `distributor_zipcode` | "98506" | The distributor zip code, if applicable. |
-    | `distributor_license_number` | "L-123" | The distributor license number, if applicable. |
-    | `producer` | "Grow House" | The producer of the sampled product. |
-    | `producer_address` | "3rd & Army, San Francisco, CA 55555" | The producer's address. |
-    | `producer_street` | "3rd & Army" | The producer's street. |
-    | `producer_city` | "San Francisco" | The producer's city. |
-    | `producer_state` | "CA" | The producer's state. |
-    | `producer_zipcode` | "55555" | The producer's zipcode. |
-    | `producer_license_number` | "L2Calc" | The producer's license number. |
-    | `product_name` | "Blue Rhino Pre-Roll" | The name of the product. |
-    | `lab_id` | "Sample-0001" | A lab-specific ID for the sample. |
-    | `product_type` | "flower" | The type of product. |
-    | `batch_number` | "Order-0001" | A batch number for the sample or product. |
-    | `traceability_ids` | ["1A4060300002199000003445"] | A list of relevant traceability IDs. |
-    | `product_size` | 2000 | The size of the product in milligrams. |
-    | `serving_size` | 1000 | An estimated serving size in milligrams. |
-    | `servings_per_package` | 2 | The number of servings per package. |
-    | `sample_weight` | 1 | The weight of the product sample in grams. |
-    | `status` | "pass" | The overall pass / fail status for all contaminant screening analyses. |
-    | `total_cannabinoids` | 14.20 | The analytical total of all cannabinoids measured. |
-    | `total_thc` | 14.00 | The analytical total of THC and THCA. |
-    | `total_cbd` | 0.20 | The analytical total of CBD and CBDA. |
-    | `total_terpenes` | 0.42 | The sum of all terpenes measured. |
-    | `strain_name` | "Blue Rhino" | A strain name, if specified. Otherwise, try to parse the `strain_name` from the `product_name`. |
-    """
-
-    # Prompt used to parse results from all pages.
-    RESULTS_PROMPT = """Given text, extract JSON. Extract only `results` as a list of JSON objects, e.g.:
-
-    {
-        "results": [
-            {
-                "analysis": str,
-                "key": str,
-                "name": str,
-                "value": float,
-                "mg_g": float,
-                "units": str,
-                "limit": float,
-                "lod": float,
-                "loq": float,
-                "status": str
-            }
-        ]
-    }
-
-    Where:
-
-    | Field | Example| Description |
-    |-------|--------|-------------|
-    | `analysis` | "pesticides" | The analysis used to obtain the result. |
-    | `key` | "pyrethrins" | A standardized key for the result analyte. |
-    | `name` | "Pyrethrins" | The lab's internal name for the result analyte |
-    | `value` | 0.42 | The value of the result. |
-    | `mg_g` | 0.00000042 | The value of the result in milligrams per gram. |
-    | `units` | "ug/g" | The units for the result `value`, `limit`, `lod`, and `loq`. |
-    | `limit` | 0.5 | A pass / fail threshold for contaminant screening analyses. |
-    | `lod` | 0.01 | The limit of detection for the result analyte. Values below the `lod` are typically reported as `ND`. |
-    | `loq` | 0.1 | The limit of quantification for the result analyte. Values above the `lod` but below the `loq` are typically reported as `<LOQ`. |
-    | `status` | "pass" | The pass / fail status for contaminant screening analyses. |
-    """
-
-
-    # Initialize OpenAI
-    os.environ['OPENAI_API_KEY'] = dotenv_values('.env')['OPENAI_API_KEY']
-    openai_api_key = os.environ['OPENAI_API_KEY']
-    client = OpenAI()
-
-    # TODO: Parse TerpLife Labs COAs.
-
-    # # Define the parsing prompt.
-    # coa_prompt = COA_PROMPT
-    # metadata_prompt = 'Text: ' + front_page_text + '\n\nJSON:'
-    # instructional_prompt = INSTRUCTIONAL_PROMPT
-    # messages = [
-    #     {'role': 'system', 'content': coa_prompt},
-    #     {'role': 'system', 'content': instructional_prompt},
-    #     {'role': 'user', 'content': metadata_prompt},
-    # ]
-    # temperature = 0.0
-    # user = 'cannlytics'
-    # max_tokens = 4_096
-
-    # # Prompt AI to extract COA metadata as JSON.
-    # completion = client.chat.completions.create(
-    #     model='gpt-4-1106-preview',
-    #     messages=messages,
-    #     max_tokens=max_tokens,
-    #     temperature=temperature,
-    #     user=user,
-    # )
-    # print(completion.choices[0].message)
-    # usage = completion.model_dump()['usage']
-    # content = completion.choices[0].message.content
-    # extracted_json = content.lstrip('```json\n').rstrip('\n```')
-    # extracted_data = json.loads(extracted_json)
-
-    # # Prompt AI to extract COA results as JSON.
-    # # FIXME: Get all results!
-    # results_prompt = RESULTS_PROMPT
-    # messages = [
-    #     {'role': 'system', 'content': results_prompt},
-    #     {'role': 'system', 'content': instructional_prompt},
-    # ]
-    # content = 'Text: ' + front_page_text #  + '\n\nList of results as JSON:'
-    # messages.append({'role': 'user', 'content': content})
-    # results_completion = client.chat.completions.create(
-    #     model='gpt-4-1106-preview',
-    #     messages=messages,
-    #     max_tokens=max_tokens,
-    #     temperature=temperature,
-    #     user=user,
-    # )
-    # print(results_completion.choices[0].message)
-    # usage = results_completion.model_dump()['usage']
-    # content = results_completion.choices[0].message.content
-    # extracted_json = content.lstrip('```json\n').rstrip('\n```')
-    # extracted_data = json.loads(extracted_json)
