6ca9672cfa0647681b82271de170231253514ca1
Keegan Skeate <keeganskeate@gmail.com>
2024-02-26 21:41:20 -0800

    Collected latest licenses | Continuing improvements to website

--- datasets/cannabis_sales/algorithms/get_sales_wa.py
+++ datasets/cannabis_sales/algorithms/get_sales_wa.py
@@ -517,7 +517,7 @@ if __name__ == '__main__':
 
     # Parameters.
     manager = CCRS()
-    first_file = 40
+    first_file = 65
     last_file = None
     reverse = False
     skip_existing = False
