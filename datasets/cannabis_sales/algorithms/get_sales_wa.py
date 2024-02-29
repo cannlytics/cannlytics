865870c718fade310df998eba0c373fab37aff39
Keegan Skeate <keeganskeate@gmail.com>
2024-02-28 22:30:56 -0800

    Added words to settings.json

--- datasets/cannabis_sales/algorithms/get_sales_wa.py
+++ datasets/cannabis_sales/algorithms/get_sales_wa.py
@@ -517,7 +517,7 @@ if __name__ == '__main__':
 
     # Parameters.
     manager = CCRS()
-    first_file = 65
+    first_file = 35
     last_file = None
     reverse = False
     skip_existing = False
