af5c3e386c30ca4e104d526c96548410afce5112
Keegan Skeate <keeganskeate@gmail.com>
2024-02-29 22:52:46 -0800

    Added new vocabulary to settings | Running get sales WA

--- datasets/cannabis_sales/algorithms/get_sales_wa.py
+++ datasets/cannabis_sales/algorithms/get_sales_wa.py
@@ -517,7 +517,7 @@ if __name__ == '__main__':
 
     # Parameters.
     manager = CCRS()
-    first_file = 35
+    first_file = 37
     last_file = None
     reverse = False
     skip_existing = False
