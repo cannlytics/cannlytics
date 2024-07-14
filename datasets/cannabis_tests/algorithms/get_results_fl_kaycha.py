639c6bb336b39312e1686b0027ea0917d029cca0
Keegan Skeate <keeganskeate@gmail.com>
2024-07-13 21:24:04 -0700

    Continuing to run / test get results

--- datasets/cannabis_tests/algorithms/get_results_fl_kaycha.py
+++ datasets/cannabis_tests/algorithms/get_results_fl_kaycha.py
@@ -46,39 +46,39 @@ import requests
 # - House of Platinum Cannabis
 # - Cookies Florida, Inc.
 FLORIDA_LICENSES = {
-    'MMTC-2015-0002': {
-        'business_dba_name': 'Ayr Cannabis Dispensary',
-        'business_legal_name': 'Liberty Health Sciences, FL',
-        'slug': 'Liberty+Health+Sciences%2C+FL',
-    },
-    'MMTC-2017-0011': {
-        'business_dba_name': 'Cannabist',
-        'slug': 'Cannabist',
-    },
-    'MMTC-2019-0018': {
-        'business_dba_name': 'Cookies Florida, Inc.',
-        'slug': '',
-    },
-    'MMTC-2015-0001': {
-        'business_dba_name': 'Curaleaf',
-        'slug': 'CURALEAF+FLORIDA+LLC',
-    },
-    'MMTC-2015-0003': {
-        'business_dba_name': 'Fluent ',
-        'slug': 'Fluent',
-    },
-    'MMTC-2019-0019': {
-        'business_dba_name': 'Gold Leaf',
-        'slug': 'Gold+Leaf',
-    },
-    'MMTC-2019-0021': {
-        'business_dba_name': 'Green Dragon',
-        'slug': 'Green+Dragon',
-    },
-    'MMTC-2016-0007': {
-        'business_dba_name': 'GrowHealthy',
-        'slug': 'GrowHealthy',
-    },
+    # 'MMTC-2015-0002': {
+    #     'business_dba_name': 'Ayr Cannabis Dispensary',
+    #     'business_legal_name': 'Liberty Health Sciences, FL',
+    #     'slug': 'Liberty+Health+Sciences%2C+FL',
+    # },
+    # 'MMTC-2017-0011': {
+    #     'business_dba_name': 'Cannabist',
+    #     'slug': 'Cannabist',
+    # },
+    # 'MMTC-2019-0018': {
+    #     'business_dba_name': 'Cookies Florida, Inc.',
+    #     'slug': '',
+    # },
+    # 'MMTC-2015-0001': {
+    #     'business_dba_name': 'Curaleaf',
+    #     'slug': 'CURALEAF+FLORIDA+LLC',
+    # },
+    # 'MMTC-2015-0003': {
+    #     'business_dba_name': 'Fluent ',
+    #     'slug': 'Fluent',
+    # },
+    # 'MMTC-2019-0019': {
+    #     'business_dba_name': 'Gold Leaf',
+    #     'slug': 'Gold+Leaf',
+    # },
+    # 'MMTC-2019-0021': {
+    #     'business_dba_name': 'Green Dragon',
+    #     'slug': 'Green+Dragon',
+    # },
+    # 'MMTC-2016-0007': {
+    #     'business_dba_name': 'GrowHealthy',
+    #     'slug': 'GrowHealthy',
+    # },
     'MMTC-2017-0013': {
         'business_dba_name': 'GTI (Rise Dispensaries)',
         'slug': 'GTI',
@@ -119,22 +119,22 @@ FLORIDA_LICENSES = {
         'business_dba_name': 'Sunnyside*',
         'slug': 'Sunnyside',
     },
-    'MMTC-2015-0004': {
-        'business_dba_name': 'Surterra Wellness',
-        'slug': 'Surterra+Wellness',
-    },
-    'MMTC-2019-0020': {
-        'business_dba_name': 'The Flowery',
-        'slug': 'The+Flowery',
-    },
-    'MMTC-2015-0005': {
-        'business_dba_name': 'Trulieve',
-        'slug': 'Trulieve',
-    },
-    'MMTC-2017-0009': {
-        'business_dba_name': 'VidaCann',
-        'slug': 'VidaCann',
-    },
+    # 'MMTC-2015-0004': {
+    #     'business_dba_name': 'Surterra Wellness',
+    #     'slug': 'Surterra+Wellness',
+    # },
+    # 'MMTC-2019-0020': {
+    #     'business_dba_name': 'The Flowery',
+    #     'slug': 'The+Flowery',
+    # },
+    # 'MMTC-2015-0005': {
+    #     'business_dba_name': 'Trulieve',
+    #     'slug': 'Trulieve',
+    # },
+    # 'MMTC-2017-0009': {
+    #     'business_dba_name': 'VidaCann',
+    #     'slug': 'VidaCann',
+    # },
 }
 
 # Define the minimum file size for a PDF.
