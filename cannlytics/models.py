c619a0b2b2507ade024d8ec894345252268f39b1
Keegan Skeate <keeganskeate@gmail.com>
2024-09-09 21:41:37 -0700

    Sketching out data models

---
+++ cannlytics/models.py
@@ -0,0 +1,35 @@
+"""
+Models
+Copyright (c) 2024 Cannlytics
+
+Authors: Keegan Skeate <https://github.com/keeganskeate>
+Created: 9/9/2024
+Updated: 9/9/2024
+License: MIT License <https://github.com/cannlytics/cannlytics/blob/main/LICENSE>
+"""
+
+# TODO: Strain model
+
+
+
+# TODO: Product model
+
+
+
+# TODO: Label model
+
+
+
+# TODO: Lab Result model
+
+
+
+# TODO: Receipt model
+
+
+
+# TODO: License model
+
+
+
+# TODO: Lab model
